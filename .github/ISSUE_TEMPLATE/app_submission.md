e4ef9392c755ec2c2cdbd9aff81a18f77753935f
Keigh Rim <keigh.rim@gmail.com>
2023-05-13 23:24:53 -0400

    updated app submittion issue template

--- .github/ISSUE_TEMPLATE/app_submission.md
+++ .github/ISSUE_TEMPLATE/app_submission.md
@@ -7,13 +7,11 @@ labels: app
 * DO NOT CHANGE the title of this issue
 
 #### Enter a public git repository address of the app below.
-* address: 
-#### Enter a git tag or commit ID of the app below.
-##### This value will be used as the version of the docker image built from the repo. Although it does not need to match the version of the app itself, it is highly recommended to do so. 
-* gittag: 
-#### Any comment you want to add. For example, a short description of the app. 
-* comment:
-
+NAME: 
+#### Enter a git tag of the app version below.
+TAG: 
+#### If the app container image is publicly available, enter the image address below.
+CONTAINER: 
 
 ---
 Fill out the above form to start a submission of a CLAMS app. 
--- scripts/parse-issue.py
+++ scripts/parse-issue.py
@@ -7,11 +7,14 @@ appcontainer = None
 for line in sys.stdin:
     line = line.strip()
     if line.startswith('NAME'):
-        apprepo = line.split(":")[1].strip()
+        apprepo = line.split(":", 1)[1].strip()
+        gh_baseurl = 'https://github.com/'
+        if apprepo.startswith(gh_baseurl):
+            apprepo = apprepo[len(gh_baseurl):]
     if line.startswith('TAG'):
-        apptag = line.split(":")[1].strip()
+        apptag = line.split(":", 1)[1].strip()
     if line.startswith('CONTAINER'):
-        appcontainer = line.split(":")[1].strip()
+        appcontainer = line.split(":", 1)[1].strip()
         
 if None in (apprepo, apptag):
     print('ERROR: missing apprepo or apptag')
@@ -23,4 +26,4 @@ with open(env_file, 'a') as github_env:
     github_env.write(f'apprepo={apprepo}\n')
     github_env.write(f'apptag={apptag}\n')
     if appcontainer:
-        github_env.write(f'apptag={appcontainer}\n')
+        github_env.write(f'appcontainer={appcontainer}\n')
